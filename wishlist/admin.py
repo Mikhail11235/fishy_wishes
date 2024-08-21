292ce2a121dd04f53e470c82817eb238710bcfc3
Mikhail Posevin <mp@workconsult.com>
2024-08-21 15:56:48 +0300

    Fix admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Item, ApiRequestLog
 
 
-admin.site.register(Item, ApiRequestLog)
+admin.site.register(Item)
+
+
+@admin.register(ApiRequestLog)
+class ApiRequestLogAdmin(admin.ModelAdmin):
+    list_display = ('api_name', 'ip_address', 'request_time')
